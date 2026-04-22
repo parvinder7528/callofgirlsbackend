3eb4de4a17ba2995c0301a4ccd647613a0c61584
Pankajkumar34 <pankajgithubnew@gmail.com>
2026-04-22 23:07:03 +0530

    file url set

--- routes/fileUploadRoutes.js
+++ routes/fileUploadRoutes.js
@@ -8,7 +8,7 @@ router.post("/upload", upload.array('photos', 8), (req, res) => {
             return res.status(400).json({ message: "No files uploaded" });
         }
         // const url = "http://localhost:4000"
-        const url="https://callofgirls.vercel.app"
+        const url="https://callofgirlsbackend.vercel.app"
         const fileUrls = req.files.map(file => {
             return `${url}/uploads/gallery/${file.filename}`;
         });
